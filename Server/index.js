bc2a24e563516558df02347a81b2098e7156bd3b
harikrishna7730 <abhitangala@gmail.com>
2025-04-21 20:45:28 +0530

    filename changed

--- Server/index.js
+++ Server/index.js
@@ -1,45 +1,21 @@
-const port = process.env.PORT || 3100;
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();
-app.use(express.json());
+const apiRoutes = require("./Routes/apis");
 const connectDB = require("./db");
 
-const allowedOrigins = [
-  "https://look-up-ashen.vercel.app",
-  "https://look-up-admin-cyan.vercel.app",
-  "https://lookup-cn6m.onrender.com", // optional: keep if you use local dev
-];
+dotenv.config();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like curl or mobile apps)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/Upload", express.static("Upload"));
 
-// Database connection with MongoDB
 connectDB();
 
-// API creation
-app.get("/", (req, res) => {
-  res.send("Express App is Running");
-});
+// Use API routes
+app.use("/api", apiRoutes);
 
-app.listen(port, (error) => {
-  if (!error) {
-    console.log("Server running on Port", port);
-  } else {
-    console.log("Error:" + error);
-  }
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
