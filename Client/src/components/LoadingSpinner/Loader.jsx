952bc060db68dba5863300673471d917b71f43b9
harikrishna7730 <abhitangala@gmail.com>
2025-04-28 15:00:12 +0530

    added lazy load

--- Client/src/components/LoadingSpinner/Loader.jsx
+++ Client/src/components/LoadingSpinner/Loader.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import { ThreeCircles } from 'react-loader-spinner'
+import "../LoadingSpinner/Loader.css"
 const Loader = () => {
   return (
     <>
-    <ThreeCircles
-  visible={true}
-  height="100"
-  width="100"
-  color="#4fa94d"
-  ariaLabel="three-circles-loading"
-  wrapperStyle={{
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-  }}  wrapperClass=""
-  />
+   <div className="loader"></div>
     </>
   )
 }
