1a345944471e68e4d0698522ecfdb4534da5bbc5
harikrishna7730 <abhitangala@gmail.com>
2025-05-05 16:24:28 +0530

    added loading content

--- Client/src/components/LoadingSpinner/Loader.jsx
+++ Client/src/components/LoadingSpinner/Loader.jsx
@@ -3,7 +3,9 @@ import "./Loader.css"
 const Loader = () => {
   return (
     <>
-   <div className="loader"></div>
+   <div style={{display:"flex",justifyContent:"center",alignItems:"center",fontFamily:"Poppins"}}>
+   <h1> Please Wait Loading....</h1>
+    </div>
     </>
   )
 }
